d1daeaa7adc394fa2ca37034412432874c1d4528
Fernando Biesdorf <biesdorf@gmail.com>
2026-07-16 20:52:37 +0200

    t16

---
+++ data_analysis/data_preparation_visualization/16-scale_numeric.py
@@ -0,0 +1,11 @@
+^#!/usr/bin/env python3
+""" module for scaling numeric features """
+from sklearn import preprocessing
+
+
+def scale_numeric(df):
+    """ standardizes MonthlyCharges and TotalCharges to mean 0, std 1 """
+    cols = ['MonthlyCharges', 'TotalCharges']
+    scaler = preprocessing.StandardScaler()
+    df[cols] = scaler.fit_transform(df[cols])
+    return df
