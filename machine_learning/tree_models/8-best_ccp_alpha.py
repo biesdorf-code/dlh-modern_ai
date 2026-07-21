b15080af3dae50d96c18482bcf14c26d8452f6e0
Fernando Biesdorf <biesdorf@gmail.com>
2026-07-21 15:53:35 +0200

    8, find the best treem based on scores

--- machine_learning/tree_models/8-best_ccp_alpha.py
+++ machine_learning/tree_models/8-best_ccp_alpha.py
@@ -6,21 +6,11 @@ def get_best_alpha(clfs, train_scores, test_scores, ccp_alphas):
     """ Returns the best alpha and its classifier """
 
     best_score = max(test_scores)
-    best_index = None
+    candidates = [i for i, score in enumerate(test_scores)
+                  if score == best_score]
 
-    for i in range(len(test_scores)):
-        if test_scores[i] != best_score:
-            continue
-        if best_index is None:
-            best_index = i
-            continue
-
-        gap = train_scores[i] - test_scores[i]
-        best_gap = train_scores[best_index] - test_scores[best_index]
-
-        if gap < best_gap:
-            best_index = i
-        elif gap == best_gap and ccp_alphas[i] > ccp_alphas[best_index]:
-            best_index = i
+    best_index = min(candidates,
+                     key=lambda i: (train_scores[i] - test_scores[i],
+                                    -ccp_alphas[i]))
 
     return ccp_alphas[best_index], clfs[best_index]
