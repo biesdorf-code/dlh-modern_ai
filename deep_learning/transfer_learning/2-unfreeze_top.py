5eb2b35249ad5e7fee999d5b651a50940f5f7408
Fernando Biesdorf <biesdorf@gmail.com>
2026-09-22 20:16:40 +0000

    t2, removed the Keras import

--- deep_learning/transfer_learning/2-unfreeze_top.py
+++ deep_learning/transfer_learning/2-unfreeze_top.py
@@ -1,15 +1,12 @@
 #!/usr/bin/env python3
 """ module for a function to unfreeze the top layers of a base model """
-from tensorflow import keras
 
 
 def unfreeze_top_layers(model, n_layers):
     """ unfreezes the last n_layers of the base model, rest stays frozen """
     base_model = model
-    if len(model.layers) > 1 and isinstance(model.layers[1], keras.Model):
+    if len(model.layers) > 1 and hasattr(model.layers[1], "layers"):
         base_model = model.layers[1]  # full pipeline: base is 2nd layer
     split = max(len(base_model.layers) - n_layers, 0)  # safe for 0 or big
-    for layer in base_model.layers[:split]:
-        layer.trainable = False
-    for layer in base_model.layers[split:]:
-        layer.trainable = True
+    for i, layer in enumerate(base_model.layers):
+        layer.trainable = i >= split
